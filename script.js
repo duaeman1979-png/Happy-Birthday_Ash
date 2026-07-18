dd8e3010ba1c21b645ae67adf9dfea7fbdee1ee9
duaeman1979-png <duaeman1979@gmail.com>
2026-07-18 18:55:14 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const surpriseBtn = document.getElementById("surpriseBtn");
+
+surpriseBtn.addEventListener("click", () => {
+    surpriseBtn.innerHTML = "❤️ Happy Birthday Ayesha ❤️";
+
+    alert("🎉 Happy Birthday Ayesha! 🎂\n\nThis is only the beginning of your surprise. ❤️");
+});
